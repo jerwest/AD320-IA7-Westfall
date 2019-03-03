ef74422f8075dee606a2052af21cd248a7cbcca2
Jeremy Westfall <jeremydwestfall@gmail.com>
2019-03-03 13:32:28 -0800

    had to rollback due to issue with server not starting

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var filmRouter = require('./routes/film');
+var usersRouter = require('./routes/users');
 
 var app = express();
 
-// // view engine setup
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -18,6 +18,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -34,8 +37,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-app.use('/', indexRouter);
-app.use('/api/v1/film', filmRouter);
 
 module.exports = app;
