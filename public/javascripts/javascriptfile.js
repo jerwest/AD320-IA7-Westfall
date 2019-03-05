eb32395a7536784376ab15f4cf41096b06e3c648
Jeremy Westfall <jeremydwestfall@gmail.com>
2019-03-05 00:16:46 -0800

    finalized js to work with pug file

--- public/javascripts/javascriptfile.js
+++ public/javascripts/javascriptfile.js
@@ -1,52 +1,11 @@
-
 window.onload = function() {
+	var div = document.getElementById("square");
 
-/***************DIV #1***************/
-var uno = document.getElementById("uno");
+	div.addEventListener("click", function(){
+		const curColour = document.getElementById("square").
+			style.backgroundColor;
 
-uno.addEventListener("click", function() {
-	alert("Hello, User!");
-});
-
-/***************DIV #2***************/
-var dos = document.getElementById("dos");
-
-window.addEventListener("resize", function(){
-  dos.innerHTML = Math.random();
-});
-
-/***************DIV #3***************/
-var tres = document.getElementById("tres");
-
-tres.addEventListener('mouseover', function(){
-       tres.style.backgroundColor = "red";
-        tres.innerHTML = "Thanks!";
-   });
-
- tres.addEventListener('mouseout', function(){
-        tres.style.backgroundColor = "";
-        tres.innerHTML = "Hover over me, to change me.";
-   });
-
-/***************DIV #4***************/
-var cuatro = document.getElementById("cuatro");
-
-cuatro.addEventListener("mouseover", function(){
-  cuatro.innerHTML += "Moused over!<br>"
-});
-
-cuatro.addEventListener("mouseout", function(){
-  cuatro.innerHTML += "Moused out!<br>"
-});
-
-/***************DIV #5***************/
-var myImage = document.getElementById("myImage");
-
-myImage.addEventListener("mousedown", function(){
-  myImage.src = "images/bulbon.gif";
-});
-
-myImage.addEventListener("mouseup", function(){
-  myImage.src = "images/bulboff.gif";
-});
+		document.getElementById("square").style.backgroundColor
+			= curColour === 'blue' ? 'red' : 'blue';
+	});
 }
