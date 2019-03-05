77de747a21d953acd56438da8a394b5103f84e0b
Jeremy Westfall <jeremydwestfall@gmail.com>
2019-03-04 21:25:51 -0800

    updated routes and route2

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var route1 = require('./routes/route1');
+var route1 = require('./routes/route2');
 
 var app = express();
 
@@ -19,8 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', route1);
+app.use('/', route2);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
