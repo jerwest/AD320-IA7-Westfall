64a9ba7ca8db17f90e7f99a24a13dc47f3f696fb
Jeremy Westfall <jeremydwestfall@gmail.com>
2019-03-05 00:17:25 -0800

    add route2.js file

---
+++ routes/route2.js
@@ -0,0 +1,71 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('route1');
+});
+
+module.exports = router;
+
+// router.get('/all', function(req, res, next) {
+//   db.query("select * from film", (error, result, fields) => {
+//     if (error) {
+//       res.status(500).send(error);
+//     }
+//     res.send(result);
+//   });
+// });
+//
+// router.get('/simple', function(req, res, next) {
+//   db.query("select film_id, title from film", (error, result, fields) => {
+//     if (error) {
+//       res.status(500).send(error);
+//     }
+//     res.send(result);
+//   });
+// });
+//
+// router.get('/rating/:rating', function(req, res, next) {
+//   var sqlQuery = "select * from film where rating=?";
+//   db.query(sqlQuery, [req.params.rating], (error, result, fields) => {
+//     if (error) {
+//       res.status(500).send(error);
+//     }
+//     res.send(result);
+//   });
+// });
+//
+//
+//
+// router.post('/addFilm', function(req,res,next){
+//   var query = "INSERT INTO film (title, description, release_year, language_id,"
+//     +" rental_duration, rental_rate, length, replacement_cost, rating) "
+//     +"VALUES ( ?, ?, ?, ?, 3, 4.99, ?, 19.99, 'G');"
+//   var queryParams = [
+//     req.body.title,
+//     req.body.description,
+//     parseInt(req.body.release_year),
+//     parseInt(req.body.language_id),
+//     parseInt(req.body.length)
+//   ];
+//
+//   db.query(query, queryParams, (error, result, fields) => {
+//     if (error){
+//       res.status(500).send(error);
+//     }
+//     res.status(201).send('record added');
+//   });
+// });
+//
+// router.get('/:id', function(req, res, next) {
+//   var sqlQuery = "select * from film where film_id=?";
+//   db.query(sqlQuery, [req.params.id], (error, result, fields) => {
+//     if (error) {
+//       res.status(500).send(error);
+//     }
+//     res.send(result);
+//   });
+// });
+//
+// module.exports = router;
