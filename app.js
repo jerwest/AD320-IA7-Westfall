05a89185cbbdfb17acd6034aa0b91ea89646c392
Jeremy Westfall <jeremydwestfall@gmail.com>
2019-03-04 22:05:14 -0800

    trying to get routes to work, updated app.use for route1

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', route1);
-app.use('/', route2);
+app.use('/route2', route2);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
